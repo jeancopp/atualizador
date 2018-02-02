ec6dc74edce6a0ef742d191150f6586445dabd14
Jean Coppieters <jean.coppieters@Hotmail.com>
2018-02-02 13:53:51 -0200

    Adequação ao jenkins

--- api/atualizador-api.js
+++ api/atualizador-api.js
@@ -1,15 +1,25 @@
 const child_process = require('child_process');
 
+const service = function(error, stdout, stderr) {
+    if(error) console.log(error);
+    console.log(stdout);
+    console.log("vou executar você");
+    resp.json({ "message":"atualização iniciada"});
+    next();
+};
+
 module.exports = function(config){
     return function(req,resp,next){
+        let parametros = req.params;
+        let tok = parametros.token;
+        let urlDownload = parametros.body.build ? parametros.body.build.full_url : "";
 
-        child_process.exec(config.batfile, function(error, stdout, stderr) {
-            if(error) console.log(error);
-            console.log(stdout);
-            console.log("vou executar você");
-            resp.json({ "teste":"teste"});
-            next();
-        });
+        if( tok && tok == config.token && urlDownload ){
+            child_process.exec(`${config.batfile} ${urlDownload}`, service);
+        }else{
+            resp.status(405).json({
+                "message" : "Token inválido"
+            });
+        }
     };
-
 };
--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const config = require("./config.json");
-
+global.config = config;
 
 const app = require("./config/server-factory.js");
 const router = require("./config/router-config.js");
 
 router(app, config);
-
 
 app.listen(config.port, () => console.log("Servidor iniciado"));
